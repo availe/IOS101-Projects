6d147d3b8c776f15ff1db1674cef866b3ce078ed
availe <94802525+availe@users.noreply.github.com>
2023-11-17 19:15:11 -0500

    revert-cocoapods-changes

--- Pronouncer/Pods/InstantSearchVoiceOverlay/Sources/InstantSearchVoiceOverlay/VoiceOverlayPackage.swift
+++
@@ -1,3 +0,0 @@
-struct VoiceOverlayPackage {
-    var text = "Hello, World!"
-}
--- Pronouncer/Pods/Target Support Files/InstantSearchVoiceOverlay/InstantSearchVoiceOverlay-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_InstantSearchVoiceOverlay : NSObject
-@end
-@implementation PodsDummy_InstantSearchVoiceOverlay
-@end
